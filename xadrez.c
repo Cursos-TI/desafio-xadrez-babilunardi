0e64d10814bae2a40ad8fbdc1361feb083ef1940
babilunardi <barbylunardi57@gmail.com>
2026-04-20 21:11:29 -0300

    xadrezinho facinho

--- xadrez.c
+++ xadrez.c
@@ -4,29 +4,43 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //torre = reta 5 casas, bispo diagonal 5 casas, rainha 8 casas pra esquerda
+    
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    int rainha = 0, bispo = 0;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    for (int i = 0; i < 5; i++) 
+    {
+        printf("Torre para direita\n");
+    }
+    
+        printf("**PROXIMA PECA**\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+//parte do while - rainha 8 casas pra esquerda
+
+    while (rainha < 8)
+    {
+        printf("Rainha para esquerda\n");
+        rainha++;
+    }
+        
+    printf("**PROXIMA PECA**\n");
+
+//do-while bispo diagonal 5
+
+
+    do 
+    {
+        printf("Bispo para Cima Direita\n");
+        bispo++;
+
+    } while (bispo < 5);
+    
 
     return 0;
 }
